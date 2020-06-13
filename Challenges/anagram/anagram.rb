8021437bca99ab7f5f73c1bbaf8886b6efb3a4c5
Alex Drover <alexfdrover@gmail.com>
2020-06-13 12:41:24 -0700

    Minor refactoring of anagram.rb for efficiency

--- Challenges/anagram/anagram.rb
+++ Challenges/anagram/anagram.rb
@@ -1,13 +1,13 @@
 
 class Anagram
   def initialize(given_word)
-    @given_word = given_word
+    @given_word = given_word.downcase
   end
 
   def match(potential_anagrams)
-    given_tally = @given_word.downcase.chars.tally
+    given_tally = @given_word.chars.tally
     potential_anagrams.select do |word|
-      (word.downcase.chars.tally == given_tally) && (word.downcase != @given_word.downcase)
+      (word.downcase.chars.tally == given_tally) && (word.downcase != @given_word)
     end
   end
 end
