eecce5de5f121242d5a77f55a7b324c4f6ef4ff8
Alex Drover <alexfdrover@gmail.com>
2020-06-13 15:05:54 -0700

    Refactoring of point_mutations.rb. Second draft.

--- Challenges/point_mutations/point_mutations.rb
+++ Challenges/point_mutations/point_mutations.rb
@@ -1,36 +1,12 @@
-require 'pry'
+
 class DNA
-  def initialize(first_strand)
-    @first_strand = first_strand
+  def initialize(strand)
+    @strand = strand
   end
 
-  def hamming_distance(second_strand)
-    @second_strand = second_strand
-    expand_strand if different_lengths?
-    count_differences
-  end
+  def hamming_distance(strand_2)
+    strand_1 = @strand[0, strand_2.length]
 
-  private
-
-  def different_lengths?
-    @first_strand.length != @second_strand.length
-  end
-
-  def expand_strand
-    max_length = [@first_strand.length, @second_strand.length].max
-    @first_strand = @first_strand.ljust(max_length, '-')
-    @second_strand = @second_strand.ljust(max_length, '-')
-  end
-
-  def count_differences
-    index = 0
-    counter = 0
-    first_chars = @first_strand.chars
-    second_chars = @second_strand.chars
-    while index < @first_strand.length
-      counter += 1 if (first_chars[index] != second_chars[index]) && first_chars[index] != '-' && second_chars[index] != '-'
-      index += 1
-    end
-    counter
+    strand_1.chars.zip(strand_2.chars).count { |pair| pair.first != pair.last }
   end
 end
