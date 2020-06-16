fa28d50f2787b14f6d64099de653c07ef104fb97
Alex Drover <alexfdrover@gmail.com>
2020-06-15 19:31:16 -0700

    Directory and code for beer_song. Code is a structural outline and does not run.

---
+++ Challenges/beer_song/beer_song.rb
@@ -0,0 +1,33 @@
+class BeerSong
+  def initialize
+    @verse_lookup = { 0 => ZeroVerse, 
+                      1 => OneVerse, 
+                      2 => TwoVerse,
+                      3..99 => DefaultVerse 
+                    }
+  end
+
+  def verse(number)
+    @verse_lookup[number].verse(number)
+  end
+end
+
+class ZeroVerse
+  def self.verse(number)
+  end
+end
+
+class OneVerse
+  def self.verse(number)
+  end
+end
+
+class TwoVerse
+  def self.verse(number)
+  end
+end
+
+class DefaultVerse
+  def self.verse(number)
+  end
+end
