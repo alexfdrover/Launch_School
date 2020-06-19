6b2b34192c246f7e6ae86edfe9dbb8559045b176
Alex Drover <alexfdrover@gmail.com>
2020-06-18 23:48:41 -0700

    Refactoring for clarity

--- Challenges/secret_handshake/secret_handshake.rb
+++ Challenges/secret_handshake/secret_handshake.rb
@@ -7,17 +7,17 @@ class SecretHandshake
 
   def commands
     return [] if validation_error?
-    binary_conversion
-    digits = @binary.chars.reverse
+    create_binary_str
+    digits = @binary_str.chars.reverse
     actions = COMMANDS.select.with_index { |_, idx| digits[idx] == '1' }
-    actions.reverse! if @binary.size == 5 && @binary[0] == '1'
+    actions.reverse! if @binary_str.size == 5 && @binary_str[0] == '1'
     actions
   end
 
   private
 
-  def binary_conversion
-    @binary = @input.to_i.to_s(2)
+  def create_binary_str
+    @binary_str = @input.to_i.to_s(2)
   end
 
   def validation_error?
