9c319b1383815ebbdb035acdac93aa26adb5b705
Alex Drover <alexfdrover@gmail.com>
2020-06-23 11:14:45 -0700

    Directory, code and tests for grade_school. All tests pass, no Rubocop offenses. First draft.

---
+++ Challenges/grade_school/grade_school.rb
@@ -0,0 +1,41 @@
+=begin
+problem
+  store student name and the grade they're in
+  add student to grade
+  return a list of all students in a grade
+  return a sorted list of all students in all grades
+    grade low-high, students in value sorted alphabetically
+  students all have first name only
+examples
+  given in test file
+algo
+  make a school object that starts empty
+  add students by name and grade
+    add them to a hash where the grade is the key
+    value is an array with all names as elements
+
+=end
+
+class School
+  def initialize
+    @school = Hash.new()
+  end
+
+  def add(name, grade)
+    @school[grade] ||= [nil]
+    @school[grade] << name
+    @school[grade].compact!
+  end
+
+  def to_h
+    @school.sort_by do |grade, students|
+      students.sort!
+      grade
+    end.to_h
+  end
+
+  def grade(grade)
+    return [] if @school[grade].nil?
+    @school[grade]
+  end
+end
