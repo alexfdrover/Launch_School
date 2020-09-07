e6e02afdecc5ad0f086b48b8708c0585e19f994c
Alex Drover <alexfdrover@gmail.com>
2020-09-06 17:10:00 -0700

    Remaining code from prep book

---
+++ JS210/preparations_exercises/test.js
@@ -0,0 +1,14 @@
+function allMatches(array, regex) {
+  return array.filter(word => regex.test(word));
+}
+
+let words = [
+  'laboratory',
+  'experiment',
+  'flab',
+  'Pans Labyrinth',
+  'elaborate',
+  'polar bear',
+];
+
+console.log(allMatches(words, /lab/)); // ['laboratory', 'flab', 'elaborate']
