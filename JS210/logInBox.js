d82c413120dfced97e359182e9e96dc75968056e
Alex Drover <alexfdrover@gmail.com>
2020-09-13 13:53:27 -0700

    Refactoring of logInBox.js

--- JS210/logInBox.js
+++ JS210/logInBox.js
@@ -5,20 +5,20 @@ inner and outer lines are "| " + " "*string.length + " |"
 center line is "| " + string + " |"
 */
 
-function linePadder(string, character) {
+function linePadder(length, character) {
   let stringContainer = '';
-  for (let i = 0; i < string.length; i += 1) {
+  for (let i = 0; i < length; i += 1) {
     stringContainer += character;
   }
   return stringContainer;
 }
 
 function logInBox(string) {
-  console.log("+-" + linePadder(string, '-') + "-+");
-  console.log("| " + linePadder(string, ' ') + " |");
+  console.log("+" + linePadder(string.length + 2, '-') + "+");
+  console.log("|" + linePadder(string.length + 2, ' ') + "|");
   console.log("| " + string + " |");
-  console.log("| " + linePadder(string, ' ') + " |");
-  console.log("+-" + linePadder(string, '-') + "-+");
+  console.log("|" + linePadder(string.length + 2, ' ') + "|");
+  console.log("+" + linePadder(string.length + 2, '-') + "+");
 }
 
 logInBox('To boldly go where no one has gone before.');
