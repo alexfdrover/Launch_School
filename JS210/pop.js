f769c5830e2b1a1156fee5e9719942f186a5be7e
Alex Drover <alexfdrover@gmail.com>
2020-09-14 20:21:45 -0700

    pop.js now handles empty arrays. push.js now accepts any amount of arguments

--- JS210/pop.js
+++ JS210/pop.js
@@ -1,10 +1,14 @@
 
 function pop(arr) {
+  if (arr.length === 0) return undefined;
+
   let val = arr[arr.length - 1];
   arr.length = arr.length - 1;
   return val;
 }
 
-let count = [1, 2, 3];
-console.log(pop(count));
-console.log(count);
+const array1 = [1, 2, 3];
+console.log(pop(array1));                        // 3
+console.log(array1);                // [1, 2]
+console.log(pop([]));                           // undefined
+console.log(pop([1, 2, ['a', 'b', 'c']]));      // ["a", "b", "c"]
--- JS210/push.js
+++ JS210/push.js
@@ -1,7 +1,15 @@
 
-function push(arr, val) {
-  arr[arr.length] = val;
+function push(arr, ...args) {
+  for (let i = 0; i < args.length; i += 1) {
+    arr[arr.length] = args[i];
+  }
+
   return arr.length;
 }
 
-console.log(push([1,2,3], 4));
+const array2 = [1, 2, 3];
+console.log(push(array2, 4, 5, 6));              // 6
+console.log(array2);                // [1, 2, 3, 4, 5, 6]
+console.log(push([1, 2], ['a', 'b']));          // 3
+console.log(push([], 1));                       // 1
+console.log(push([]));                          // 0
