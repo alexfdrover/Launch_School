7822b46697d5734fd17e3aaffd00f2f730a12a39
Alex Drover <alexfdrover@gmail.com>
2020-09-14 20:10:46 -0700

    refactored concat.js to take any number of arguments

--- JS210/concat.js
+++ JS210/concat.js
@@ -1,5 +1,5 @@
 
-function concat(array1, item2) {
+function concat(array1, ...args) {
   let newArray = [];
   let length1 = array1.length;
 
@@ -7,18 +7,19 @@ function concat(array1, item2) {
     newArray.push(array1[i]);
   }
 
-  if (!Array.isArray(item2)) {
-    newArray.push(item2);
-  } else {
-    for (let j = 0; j < item2.length; j += 1) {
-      newArray.push(item2[j]);
+  for (let k = 0; k < args.length; k += 1) {
+    if (!Array.isArray(args[k])) {
+      newArray.push(args[k]);
+    } else {
+      for (let j = 0; j < args[k].length; j += 1) {
+        newArray.push(args[k][j]);
+      }
     }
   }
 
   return newArray;
 }
 
-console.log(concat([1, 2, 3], [4, 5, 6]));          // [1, 2, 3, 4, 5, 6]
-console.log(concat([1, 2], 3));                     // [1, 2, 3]
-console.log(concat([2, 3], ['two', 'three']));      // [2, 3, "two", "three"]
-console.log(concat([2, 3], 'four'));                // [2, 3, "four"]
+console.log(concat([1, 2, 3], [4, 5, 6], [7, 8, 9]));    // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(concat([1, 2], 'a', ['one', 'two']));        // [1, 2, "a", "one", "two"]
+console.log(concat([1, 2], ['three'], 4));               // [1, 2, "three", 4]
