a5d39b44202bdca1b89a65088205f656b3eac55c
Alex Drover <alexfdrover@gmail.com>
2020-09-14 20:00:50 -0700

    refactored concat.js to take non-arrays as a second parameter

--- JS210/concat.js
+++ JS210/concat.js
@@ -1,17 +1,24 @@
 
-function concat(array1, array2) {
+function concat(array1, item2) {
   let newArray = [];
+  let length1 = array1.length;
 
-  let totalLength = array1.length + array2.length;
-  for (let i = 0; i < totalLength; i += 1) {
-    if (i < array1.length) {
-      newArray.push(array1[i]);
-    } else {
-      newArray.push(array2[i - array1.length]);
+  for (let i = 0; i < length1; i += 1) {
+    newArray.push(array1[i]);
+  }
+
+  if (!Array.isArray(item2)) {
+    newArray.push(item2);
+  } else {
+    for (let j = 0; j < item2.length; j += 1) {
+      newArray.push(item2[j]);
     }
   }
 
   return newArray;
 }
 
-console.log(concat([1, 2, 3], [4, 5, 6]));       // [ 1, 2, 3, 4, 5, 6 ]
+console.log(concat([1, 2, 3], [4, 5, 6]));          // [1, 2, 3, 4, 5, 6]
+console.log(concat([1, 2], 3));                     // [1, 2, 3]
+console.log(concat([2, 3], ['two', 'three']));      // [2, 3, "two", "three"]
+console.log(concat([2, 3], 'four'));                // [2, 3, "four"]
