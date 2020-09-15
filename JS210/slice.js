000cbc1312fbcebec8abdc9d0481be6b436eeb68
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-09-15 08:29:37 -0700

    slice.js now handles wider range of permissible input values

--- JS210/slice.js
+++ JS210/slice.js
@@ -1,5 +1,8 @@
 
 function slice(array, start, end) {
+  if (start > end) [start, end] = [0, 0];
+  if (end > array.length) end = array.length
+
   let arrayContainer = [];
   for (let i = start; i < end; i += 1) {
     arrayContainer.push(array[i]);
@@ -8,5 +11,11 @@ function slice(array, start, end) {
   return arrayContainer;
 }
 
-console.log(slice([1, 2, 3, 4, 5], 0, 2));                      // [ 1, 2 ]
-console.log(slice(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 1, 3));  // [ 'b', 'c' ]
+console.log(slice([1, 2, 3], 1, 2));               // [2]
+console.log(slice([1, 2, 3], 2, 0));               // []
+console.log(slice([1, 2, 3], 5, 1));               // []
+console.log(slice([1, 2, 3], 0, 5));               // [1, 2, 3]
+
+const arr1 = [1, 2, 3];
+console.log(slice(arr1, 1, 3));                     // [2, 3]
+console.log(arr1);                                  // [1, 2, 3]
