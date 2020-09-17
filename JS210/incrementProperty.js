a520c2e35fb2f471341c748c8a3741594649a2d7
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-09-16 20:05:53 -0700

    incrementProperty.js

---
+++ JS210/incrementProperty.js
@@ -0,0 +1,21 @@
+function objectHasProperty(obj, str) {
+  return obj[str] === undefined ? false : true;
+}
+
+function incrementProperty(obj, str) {
+  if (objectHasProperty(obj, str)) {
+    return obj[str] += 1;
+  } else {
+    return obj[str] = 1;
+  }
+}
+
+let wins = {
+  steve: 3,
+  susie: 4,
+};
+
+console.log(incrementProperty(wins, 'susie'));   // 5
+console.log(wins);                               // { steve: 3, susie: 5 }
+console.log(incrementProperty(wins, 'lucy'));    // 1
+console.log(wins);                               // { steve: 3, susie: 5, lucy: 1 }
