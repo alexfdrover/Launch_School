e5e343ae06adf52dc99c9638cf8981ad1db32e1c
Alex Drover <alexfdrover@gmail.com>
2020-09-19 21:31:58 -0700

    integerToString.js

--- JS210/integerToString.js
+++ JS210/integerToString.js
@@ -1,16 +1,14 @@
 
 function integerToString(int) {
-  if (int === 0) return '0';
-  
   let validDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   let stringContainer = '';
   
-  while (int > 0) {
+  do {
     let lastDigit = int % 10;
     int = Math.floor(int / 10);
 
     stringContainer = validDigits[lastDigit] + stringContainer;
-  }
+  } while (int > 0)
 
   return stringContainer;
 }
