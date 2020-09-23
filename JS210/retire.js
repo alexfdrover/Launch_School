8931eceddcf056b14f36271dd3b6589b41857287
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-09-23 15:49:27 -0700

    retire.js

---
+++ JS210/retire.js
@@ -0,0 +1,13 @@
+
+let rlSync = require('readline-sync');
+
+let age = Number(rlSync.question('What is your age? '));
+let retirementAge = Number(rlSync.question('At what age would you like to retire? '));
+
+const today = new Date();
+const currentYear = today.getFullYear();
+
+console.log(`It's ${currentYear}. You will retire in ${currentYear + retirementAge - age}`);
+console.log(`You only have ${retirementAge - age} years of work to go!`);
+
+
