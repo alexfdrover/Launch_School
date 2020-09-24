dd85fdb093b99241e6e955f5823978f3de7170f4
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-09-24 15:25:33 -0700

    repeater.js

---
+++ JS210/repeater.js
@@ -0,0 +1,12 @@
+function repeater(string) {
+  let newString = '';
+  for (let i = 0; i < string.length; i += 1) {
+    newString += string[i] + string[i];
+  }
+
+  return newString;
+}
+
+console.log(repeater('Hello'));        // "HHeelllloo"
+console.log(repeater('Good job!'));    // "GGoooodd  jjoobb!!"
+console.log(repeater(''));             // ""
