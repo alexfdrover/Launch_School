4b314900f4d3eaab0a4a02c5e971868fb73702e0
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-09-24 15:12:56 -0700

    countOccurences.js

--- JS210/countOccurences.js
+++ JS210/countOccurences.js
@@ -1,3 +1,9 @@
+function logOccurences(object) {
+  for (let item in object) {
+    console.log(`${item} => ${String(object[item])}`)
+  }
+}
+
 function countOccurrences(array) {
   let objContainer = {};
   for (let i = 0; i < array.length; i += 1) {
@@ -6,13 +12,13 @@ function countOccurrences(array) {
     objContainer[currentObj] += 1;
   }
 
-  return objContainer;
+  logOccurences(objContainer);
 }
 
 const vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck',
                 'motorcycle', 'motorcycle', 'car', 'truck'];
 
-console.log(countOccurrences(vehicles));
+countOccurrences(vehicles);
 
 // console output
 // car => 4
