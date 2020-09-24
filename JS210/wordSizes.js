41fccf6fad088d661a26d6c227e4918b8440fd8d
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-09-23 21:32:31 -0700

    wordSizes.js

--- JS210/wordSizes.js
+++ JS210/wordSizes.js
@@ -1,18 +1,31 @@
+function sanitizeWord(word) {
+  word = word.toLowerCase();
+  
+  let cleanedWord = '';
+  for (let i = 0; i < word.length; i += 1) {
+    if (word[i] >= 'a' && word[i] <= 'z') {
+      cleanedWord += word[i];
+    }
+  }
+
+  return cleanedWord;
+}
 
 function wordSizes(sentence) {
   let wordObject = {};
   if (sentence === '') return wordObject;
-  
+
   let wordArray = sentence.split(' ');
   for (let i = 0; i < wordArray.length; i += 1) {
-    let length = wordArray[i].length;
+    let newWord = sanitizeWord(wordArray[i]);
+    let length = newWord.length;
     wordObject[length] ? wordObject[length] += 1 : wordObject[length] = 1;
   }
 
   return wordObject;
 }
 
-console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
-console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 1, "7": 2 }
-console.log(wordSizes("What's up doc?"));                              // { "2": 1, "4": 1, "6": 1 }
+console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 2 }
+console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 3 }
+console.log(wordSizes("What's up doc?"));                              // { "5": 1, "2": 1, "3": 1 }
 console.log(wordSizes(''));                                            // {}
