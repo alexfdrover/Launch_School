1904a0e3a37311f27b6d8ed0d5546cdb7a183b2f
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-09-24 15:21:58 -0700

    average.js

---
+++ JS210/average.js
@@ -0,0 +1,9 @@
+function average(intArray) {
+  let sum = 0;
+  intArray.forEach(element => sum += element);
+
+  return Math.floor(sum / intArray.length);
+}
+
+console.log(average([1, 5, 87, 45, 8, 8]));       // 25
+console.log(average([9, 47, 23, 95, 16, 52]));    // 40
