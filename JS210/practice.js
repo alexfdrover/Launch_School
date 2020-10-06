dcb6b7030055cb4e28b54c5dd5e42e1348400967
Alex Drover <alexfdrover@gmail.com>
2020-10-05 21:38:13 -0700

    JS210 cleanup

---
+++ JS210/practice.js
@@ -0,0 +1,9 @@
+let student = {
+  firstName: 'Alex',
+  lastName: 'Drover',
+  semester: 'Autumn',
+  dateOfBirth: new Date('September 28, 1993'),
+  grades: {math: 90, physics: 95, english: 98},
+};
+
+console.log(student);
