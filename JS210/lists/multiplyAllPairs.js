5ffa1228d518b42e692bfcfe70daf13a54511b41
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-10-11 12:33:53 -0700

    multiplyAllPairs.js

--- JS210/lists/multiplyAllPairs.js
+++ JS210/lists/multiplyAllPairs.js
@@ -1,14 +1,4 @@
 function multiplyAllPairs(arr1, arr2) {
-  function numSort(a, b) {
-    if (Number(a) < Number(b)) {
-      return -1;
-    } else if (Number(a) > Number(b)) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-
   let result = [];
   for (let i = 0; i < arr1.length; i += 1) {
     for (let j = 0; j < arr2.length; j += 1) {
@@ -16,7 +6,7 @@ function multiplyAllPairs(arr1, arr2) {
     }
   }
 
-  return result.sort(numSort);
+  return result.sort((a, b) => a - b);
 }
 
 console.log(multiplyAllPairs([2, 4], [4, 3, 1, 2]));    // [2, 4, 4, 6, 8, 8, 12, 16]
