c5596e756f4ee8312e95fd30b6d276e381bedba0
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-10-14 02:25:05 -0700

    staggeredCase.js

--- LS215/lesson_2/small_problems/staggeredCase.js
+++ LS215/lesson_2/small_problems/staggeredCase.js
@@ -1,16 +1,34 @@
+// function staggeredCase(str) {
+//   return str
+//      .split('')
+//      .map((char, index) => {
+//        if (index % 2 === 0) {
+//          return char.toUpperCase();
+//        } else {
+//          return char.toLowerCase();
+//        }
+//      })
+//      .join('');
+// }
+
 function staggeredCase(str) {
+  let state = 1;
   return str
-     .split('')
-     .map((char, index) => {
-       if (index % 2 === 0) {
-         return char.toUpperCase();
-       } else {
-         return char.toLowerCase();
-       }
-     })
-     .join('');
+    .split('')
+    .map(char => {
+      if ((/[^a-z]/gi).test(char)) {
+        return char;
+      } else if (state > 0) {
+        state *= -1;
+        return char.toUpperCase();
+      } else {
+        state *= -1;
+        return char.toLowerCase();
+      }
+    })
+    .join('');
 }
 
-console.log(staggeredCase('I Love Launch School!'));        // "I LoVe lAuNcH ScHoOl!"
-console.log(staggeredCase('ALL_CAPS'));                     // "AlL_CaPs"
-console.log(staggeredCase('ignore 77 the 4444 numbers'));   // "IgNoRe 77 ThE 4444 nUmBeRs"
+console.log(staggeredCase('I Love Launch School!'));        // "I lOvE lAuNcH sChOoL!"
+console.log(staggeredCase('ALL CAPS'));                     // "AlL cApS"
+console.log(staggeredCase('ignore 77 the 444 numbers'));    // "IgNoRe 77 ThE 444 nUmBeRs"
