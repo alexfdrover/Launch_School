fdc476e33f05b100160bbb7baa1bf4c02d3d39af
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-10-22 22:34:59 -0700

    fibonacci.js added memoization

--- LS215/lesson_3/small_problems/fibonacci.js
+++ LS215/lesson_3/small_problems/fibonacci.js
@@ -1,11 +1,5 @@
 /*
-Recursive Implementation
-function fibonacci(n) {
-  if (n <= 2) return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
-*/
-
+Procedural Approach
 function fibonacci(n) {
   if (n === 1 || n === 2) return 1;
 
@@ -16,7 +10,27 @@ function fibonacci(n) {
 
   return sequence[sequence.length - 1];
 }
+*/
 
+// Recursive approach with memoization
+let memos = [0, 1, 1];
+
+function fibonacci(n) {
+  if (n === 1 || n === 2) return 1;
+  if (memos[n]) {
+    return memos[n];
+  } else {
+    memos.push(fibonacci(n - 1) + fibonacci(n - 2));
+    return memos[n];
+  }
+}
+console.log(fibonacci(1));       // 1
+console.log(fibonacci(2));       // 1
+console.log(fibonacci(3));       // 2
+console.log(fibonacci(4));       // 3
+console.log(fibonacci(5));       // 5
+console.log(fibonacci(12));      // 144
+console.log(fibonacci(20));      // 6765
 console.log(fibonacci(20));       // 6765
 console.log(fibonacci(50));       // 12586269025
 console.log(fibonacci(75));       // 2111485077978050
