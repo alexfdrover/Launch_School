60601a3c3a030943d7bfe9dafc83e9f5916770b2
Alex Drover <alex@Alexs-MacBook-Pro.local>
2020-10-22 22:18:31 -0700

    restored recursive fibonacci

--- LS215/lesson_3/small_problems/fibonacci.js
+++ LS215/lesson_3/small_problems/fibonacci.js
@@ -1,3 +1,10 @@
+/*
+Recursive Implementation
+function fibonacci(n) {
+  if (n <= 2) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+*/
 
 function fibonacci(n) {
   if (n === 1 || n === 2) return 1;
