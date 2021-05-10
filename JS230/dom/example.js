44736bbbc725129b87246c153d11d62008592ee9
Alex Drover <alexfdrover@gmail.com>
2021-05-09 18:05:17 -0700

    update index.html and example.js to reflect updated lesson practice

--- JS230/dom/example.js
+++ JS230/dom/example.js
@@ -1,24 +1,6 @@
-function walk(node, callback) {
-  callback(node);
+let paragraph = document.createElement('p');
+paragraph.textContent = 'This is a test.';
+document.body.appendChild(paragraph);
 
-  for (let index = 0; index < node.childNodes.length; index += 1) {
-    walk(node.childNodes[index], callback);
-  }
-}
-
-let html = document.childNodes[1]; // skip doctype
-let body = html.lastChild;         // skip head and text nodes
-let heading = body.childNodes[1];  // skip text node
-heading.style.color = 'red';
-heading.style.fontSize = '48px';
-
-let first = true;
-walk(document, node => {
-  if (node.nodeName === "P") {
-    if (first) {
-      first = false;
-    } else {
-      node.classList.add('stanza');
-    }
-  }
-});
+let paragraph2 = paragraph.cloneNode(true);
+document.body.appendChild(paragraph2);
