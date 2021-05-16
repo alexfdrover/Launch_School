829056c1af8043d4bcdd8c0e3a4cc6f0c893afc5
Alex Drover <alexfdrover@gmail.com>
2021-05-16 12:57:07 -0700

    add code for part 1 of guess-a-word project

--- JS230/libraries/guess-a-word/javascripts/game.js
+++ JS230/libraries/guess-a-word/javascripts/game.js
@@ -1,10 +1,52 @@
-let randomWord = (function () {
-  let words = ['apple', 'orange', 'pear', 'banana'];
+document.addEventListener('DOMContentLoaded', event => {
+  const message = document.querySelector('#message');
+  const letters = document.querySelector('#spaces');
+  const guesses = document.querySelector('#guesses');
+  const apples = document.querySelector('#apples');
+  const replay = document.querySelector('#replay');
 
-  return function() {
-    let randIdx = Math.floor(Math.random() * words.length);
-    if (words.length === 0) return undefined;
-    return words.splice(randIdx, 1);
+
+  let randomWord = (function () {
+    let words = ['apple', 'orange', 'pear', 'banana'];
+  
+    return function() {
+      let randIdx = Math.floor(Math.random() * words.length);
+      if (words.length === 0) return undefined;
+      return words.splice(randIdx, 1)[0];
+    };
+  })();
+
+  function Game() {
+    this.incorrect = 0;
+    this.letters_guessed = [];
+    this.correct_spaces = 0;
+    this.word = randomWord();
+    if (!this.word) {
+      this.displayMessage("Sorry, I've run out of words!");
+      return this;
+    }
+    this.word = this.word.split("");
+    this.init();
+  }
+
+  Game.prototype = {
+    createBlanks: function() {
+      let spaces = (new Array(this.word.length + 1)).join("<span></span>");
+
+      let spans = letters.querySelectorAll("span");
+      spans.forEach(span => {
+        span.parentNode.removeChild(span);
+      });
+      letters.insertAdjacentHTML('beforeend', spaces);
+      this.spaces = document.querySelectorAll("#spaces span");
+    },
+    init() {
+      this.createBlanks();
+    },
+    displayMessage(text) {
+      message.text = text;
+    }
   };
-})();
 
+  new Game();
+});
